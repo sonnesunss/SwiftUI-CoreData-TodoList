aea63041b4fff86bbe1b5240fc94950a76e758cb
sun <145575806+sonnesunss@users.noreply.github.com>
2025-02-06 12:07:57 +0800

    init xcode project
    初始化TodoList项目

---
+++ swiftui_coredata_todolist/swiftui_coredata_todolist/swiftui_coredata_todolistApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_coredata_todolistApp.swift
+//  swiftui_coredata_todolist
+//
+//  Created by Dan Dan on 2/6/25.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_coredata_todolistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
